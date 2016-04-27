9d1400c33ee3ca801965c8a19fc8c96a3f68ac95
madhusudhand <madhusudhan.dollu@gmail.com>
2016-04-27 14:51:50 +0530

    support for gulp task runner

--- blueprints/gulp/gulpfile.js
+++ blueprints/gulp/gulpfile.js
@@ -2,25 +2,75 @@ var config      = require('./build.config.json');
 var gulp        = require('gulp');
 var browserSync = require('browser-sync').create();
 var jade        = require('gulp-jade');
+var sass        = require('gulp-sass');
+var csso        = require('gulp-csso');
+var ts          = require('gulp-typescript');
+var newer       = require('gulp-newer');
+var watch       = require('gulp-watch');
 
-gulp.task('jade', function() {
-  var YOUR_LOCALS = {};
-
-  gulp.src('./src/*.jade')
+gulp.task('jade:dev', function() {
+  gulp.src(config.app_files.jade, {base: config.app_dir + '/'})
+    .pipe(newer('./' + config.build_dir))
     .pipe(jade({
-      locals: YOUR_LOCALS
+      pretty: true
     }))
-    .pipe(gulp.dest('./dist/'))
+    .pipe(gulp.dest(config.build_dir));
 });
 
-gulp.task('dev', []);
-gulp.task('dist', []);
+gulp.task('jade:dist', function() {
+  gulp.src(config.app_files.jade, {cwd: config.app_dir + '/'})
+    .pipe(jade({
+      pretty: false
+    }))
+    .pipe(gulp.dest('./' + config.build_dir))
+});
+
+gulp.task('sass:dev', function () {
+  gulp.src(config.app_files.sass, {cwd: config.app_dir + '/'})
+    // .pipe(concat(config.app_files.css_file + '.min.css'))
+    .pipe(sass().on('error', sass.logError))
+    .pipe(gulp.dest('./' + config.build_dir));
+});
+
+gulp.task('sass:dist', function () {
+  gulp.src(config.app_files.sass, {base: config.app_dir + '/'})
+    // .pipe(concat(config.app_files.css_file + '.min.css'))
+    .pipe(sass().on('error', sass.logError))
+    .pipe(csso())
+    .pipe(gulp.dest('./' + config.build_dir));
+});
+
+var tsProject = ts.createProject('./src/tsconfig.json');
+
+gulp.task('ts', function () {
+  tsProject.src()
+    // .pipe(sourcemaps.init())
+    .pipe(ts(tsProject))
+    // .pipe(sourcemaps.write());
+    .js
+    .pipe(gulp.dest('dist'));
+});
+
+
+
+gulp.task('dev', ['jade:dev', 'sass:dev', 'ts']);
+gulp.task('dist', ['jade:dist', 'sass:dist', 'ts']);
 gulp.task('serve', ['browser-sync', 'watch']);
 gulp.task('serve', function() {
-    browserSync.init({
-        server: './' + config.build_dir
-    });
+  browserSync.init({
+    server: './' + config.build_dir,
+    notify: false,
+    ghostMode: {
+      clicks: true,
+      location: true,
+      forms: true,
+      scroll: false
+    }
+  });
 
-    // gulp.watch("app/scss/*.scss", ['sass']);
-    // gulp.watch("app/*.html").on('change', browserSync.reload);
+  gulp.watch([config.app_dir + '/*.jade', config.app_dir + '/**/*.jade'], ['jade:dev']);
+  gulp.watch([config.app_dir + '/*.scss', config.app_dir + '/**/*.scss'], ['sass:dev']);
+  gulp.watch([config.app_dir + '/*.ts', config.app_dir + '/**/*.ts'], ['ts']);
+
+  gulp.on('stop', browserSync.reload);
 });
