7e3c4d176dc18c96795a16f5f6d3ea358411ca92
madhusudhand <madhusudhan.dollu@gmail.com>
2016-05-08 13:23:59 +0530

    strict mode

---
+++ blueprints/common/src/public/auth/login.component/login.component.ts
@@ -0,0 +1,13 @@
+import {Component, OnInit} from 'angular2/core';
+
+@Component({
+    moduleId: __moduleName,
+    selector: 'login',
+    templateUrl: 'login.component.html',
+    styleUrls: ['login.component.css'],
+})
+export class LoginComponent implements OnInit {
+  constructor(){}
+
+  ngOnInit(){}
+}
