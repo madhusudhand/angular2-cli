e9e93bea76cb300335494fe77d618762292209e6
madhusudhand <madhusudhan.dollu@gmail.com>
2016-05-11 23:21:58 +0530

    setting up scaffolds

---
+++ blueprints/scaffolds/component/__name__.component.ts
@@ -0,0 +1,16 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  moduleId: __moduleName,
+  selector: '<%= selector %>',
+  templateUrl: '<%= dashedComponentName %>.component.html',
+  styleUrls: ['<%= dashedComponentName %>.component.css']
+})
+export class <%= componentName %>Component implements OnInit {
+
+  constructor() {}
+
+  ngOnInit() {
+  }
+
+}
